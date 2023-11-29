0f7cbb40569bfbd453fe759e2828f62b226b7f95
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-29 22:16:51 +0530

    @RequestMapping(value = "/test",method = RequestMethod.GET)

--- src/main/java/com/bhas/controller/TestController.java
+++ src/main/java/com/bhas/controller/TestController.java
@@ -1,7 +1,12 @@
 package com.bhas.controller;
 
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+import org.hibernate.type.descriptor.java.LocalDateTimeJavaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,5 +14,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/jwt")
 public class TestController 
 {
-
+	// test endPoint,
+	@RequestMapping(value = "/test",method = RequestMethod.GET)
+	public DayOfWeek getDate()
+	{
+		return LocalDateTime.now().getDayOfWeek();
+	}
 }
