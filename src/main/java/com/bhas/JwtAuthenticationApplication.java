25d7a32eeee057f4f794d08d3cd7fda0290d6b5e
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-29 15:57:29 +0530

    docker daemon is not running

--- src/main/java/com/bhas/JwtAuthenticationApplication.java
+++ src/main/java/com/bhas/JwtAuthenticationApplication.java
@@ -10,7 +10,7 @@ public class JwtAuthenticationApplication
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(JwtAuthenticationApplication.class, args);
-		System.out.println("\n JsonWebToken");
+		System.out.println("\n JsonWebToken \n");
 	}
 
 }
