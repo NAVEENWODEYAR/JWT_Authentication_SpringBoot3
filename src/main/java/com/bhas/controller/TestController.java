083156a1c5a0e8f3806e513063c57ebc50a1f20b
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-29 22:10:41 +0530

    @Controller

---
+++ src/main/java/com/bhas/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.bhas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+@RequestMapping("/jwt")
+public class TestController 
+{
+
+}
